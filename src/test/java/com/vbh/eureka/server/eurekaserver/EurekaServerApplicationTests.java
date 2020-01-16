13beed929b718f255037b9c7cc2ab2afe5a97ea6
Jerry-heyutong <heyutong.jerry@vbhledger.com>
2020-01-16 10:06:56 +0800

    init

---
+++ src/test/java/com/vbh/eureka/server/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vbh.eureka.server.eurekaserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
